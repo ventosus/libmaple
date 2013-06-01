21adffebda8de85704d1dcbeb75ac130c5a439b9
Hanspeter Portner <agenthp@users.sf.net>
2013-06-01 20:42:06 +0200

    F3-port: I2C peripheral

--- libmaple/include/libmaple/i2c.h
+++ libmaple/include/libmaple/i2c.h
@@ -3,6 +3,7 @@
  *
  * Copyright (c) 2010 Perry Hung.
  * Copyright (c) 2012 LeafLabs, LLC.
+ * Copyright (c) 2013 OpenMusicKontrollers.
  *
  * Permission is hereby granted, free of charge, to any person
  * obtaining a copy of this software and associated documentation
@@ -27,6 +28,7 @@
 
 /**
  * @file libmaple/include/libmaple/i2c.h
+ * @author F3-port by Hanspeter Portner <dev@open-music-kontrollers.ch>
  * @brief Inter-Integrated Circuit (I2C) peripheral support
  *
  * Currently master-only. Usage notes:
@@ -72,19 +74,6 @@ extern "C" {
 #include <libmaple/nvic.h>
 #include <libmaple/gpio.h>
 
-/** I2C register map type */
-typedef struct i2c_reg_map {
-    __io uint32 CR1;            /**< Control register 1 */
-    __io uint32 CR2;            /**< Control register 2 */
-    __io uint32 OAR1;           /**< Own address register 1 */
-    __io uint32 OAR2;           /**< Own address register 2 */
-    __io uint32 DR;             /**< Data register */
-    __io uint32 SR1;            /**< Status register 1 */
-    __io uint32 SR2;            /**< Status register 2 */
-    __io uint32 CCR;            /**< Clock control register */
-    __io uint32 TRISE;          /**< TRISE (rise time) register */
-} i2c_reg_map;
-
 /**
  * @brief I2C message type
  */
@@ -104,106 +93,17 @@ typedef struct i2c_msg {
     uint8 *data;                /**< Data */
 } i2c_msg;
 
-/*
- * Register bit definitions
- */
-
-/* Control register 1 */
-
-#define I2C_CR1_SWRST           (1U << 15)    // Software reset
-#define I2C_CR1_ALERT           (1U << 13)    // SMBus alert
-#define I2C_CR1_PEC             (1U << 12)    // Packet error checking
-#define I2C_CR1_POS             (1U << 11)    // Acknowledge/PEC position
-#define I2C_CR1_ACK             (1U << 10)    // Acknowledge enable
-#define I2C_CR1_STOP            (1U << 9)     // Stop generation
-#define I2C_CR1_START           (1U << 8)     // Start generation
-#define I2C_CR1_NOSTRETCH       (1U << 7)     // Clock stretching disable
-#define I2C_CR1_ENGC            (1U << 6)     // General call enable
-#define I2C_CR1_ENPEC           (1U << 5)     // PEC enable
-#define I2C_CR1_ENARP           (1U << 4)     // ARP enable
-#define I2C_CR1_SMBTYPE         (1U << 3)     // SMBus type
-#define I2C_CR1_SMBTYPE_DEVICE  (0U << 3)     //     SMBus type: device
-#define I2C_CR1_SMBTYPE_HOST    (1U << 3)     //     SMBus type: host
-#define I2C_CR1_SMBUS           (1U << 1)     // SMBus mode
-#define I2C_CR1_SMBUS_I2C       (0U << 1)     //     SMBus mode: I2C
-#define I2C_CR1_SMBUS_SMBUS     (1U << 1)     //     SMBus mode: SMBus
-#define I2C_CR1_PE              (1U << 0)     // Peripheral Enable
-
-/* Control register 2 */
-
-#define I2C_CR2_LAST            (1U << 12)    // DMA last transfer
-#define I2C_CR2_DMAEN           (1U << 11)    // DMA requests enable
-#define I2C_CR2_ITBUFEN         (1U << 10)    // Buffer interrupt enable
-#define I2C_CR2_ITEVTEN         (1U << 9)     // Event interupt enable
-#define I2C_CR2_ITERREN         (1U << 8)     // Error interupt enable
-#define I2C_CR2_FREQ            0x3F          // Peripheral input frequency
-
-/* Own address register 1 */
-
-#define I2C_OAR1_ADDMODE        (1U << 15)    // Addressing mode
-#define I2C_OAR1_ADDMODE_7_BIT  (0U << 15)    //     Addressing mode: 7-bit
-#define I2C_OAR1_ADDMODE_10_BIT (1U << 15)    //     Addressing mode: 10-bit
-#define I2C_OAR1_ADD            0x3FF         // Interface address
-
-/* Own address register 2 */
-
-#define I2C_OAR2_ADD2           0xFE          // Interface address
-#define I2C_OAR2_ENDUAL         1U            // Dual addressing mode enable
-
-/* Status register 1 */
-
-#define I2C_SR1_SMBALERT        (1U << 15)    // SMBus alert
-#define I2C_SR1_TIMEOUT         (1U << 14)    // Timeout or Tlow error
-#define I2C_SR1_PECERR          (1U << 12)    // PEC Error in reception
-#define I2C_SR1_OVR             (1U << 11)    // Overrun/underrun
-#define I2C_SR1_AF              (1U << 10)    // Acknowledge failure
-#define I2C_SR1_ARLO            (1U << 9)     // Arbitration lost
-#define I2C_SR1_BERR            (1U << 8)     // Bus error
-#define I2C_SR1_TXE             (1U << 7)     // Data register empty
-#define I2C_SR1_RXNE            (1U << 6)     // Data register not empty
-#define I2C_SR1_STOPF           (1U << 4)     // Stop detection
-#define I2C_SR1_ADD10           (1U << 3)     // 10-bit header sent
-#define I2C_SR1_BTF             (1U << 2)     // Byte transfer finished
-#define I2C_SR1_ADDR            (1U << 1)     // Address sent/matched
-#define I2C_SR1_SB              (1U << 0)     // Start bit
-
-/* Status register 2 */
-
-#define I2C_SR2_PEC             0xFF00        // Packet error checking register
-#define I2C_SR2_DUALF           (1U << 7)     // Dual flag
-#define I2C_SR2_SMBHOST         (1U << 6)     // SMBus host header
-#define I2C_SR2_SMBDEFAULT      (1U << 5)     // SMBus device default address
-#define I2C_SR2_GENCALL         (1U << 4)     // General call address
-#define I2C_SR2_TRA             (1U << 2)     // Transmitter/receiver
-#define I2C_SR2_BUSY            (1U << 1)     // Bus busy
-#define I2C_SR2_MSL             (1U << 0)     // Master/slave
-
-/* Clock control register */
-
-#define I2C_CCR_FS              (1U << 15)    // Fast mode selection
-#define I2C_CCR_DUTY            (1U << 14)    // Fast mode duty cycle
-#define I2C_CCR_DUTY_2_1        (0U << 14)    //     Fast mode duty: 2/1
-#define I2C_CCR_DUTY_16_9       (1U << 14)    //     Fast mode duty: 16/9
-#define I2C_CCR_CCR             0xFFF         // Clock control bits
-
-/*
- * Convenience routines
- */
-
-/* Main I2C API */
-
 /* I2C enable options */
 #define I2C_FAST_MODE           0x1           // 400 khz
 #define I2C_DUTY_16_9           0x2           // 16/9 duty ratio
 /* Flag 0x4 is reserved; DO NOT USE. */
 #define I2C_BUS_RESET           0x8           // Perform a bus reset
-void i2c_master_enable(i2c_dev *dev, uint32 flags);
 
+/* I2C error flags */
 #define I2C_ERROR_PROTOCOL      (-1)
 #define I2C_ERROR_TIMEOUT       (-2)
-int32 i2c_master_xfer(i2c_dev *dev, i2c_msg *msgs, uint16 num, uint32 timeout);
 
-void i2c_bus_reset(const i2c_dev *dev);
+/* Main I2C API */
 
 /**
  * @brief Disable an I2C device
@@ -218,41 +118,85 @@ static inline void i2c_disable(i2c_dev *dev) {
     dev->state = I2C_STATE_DISABLED;
 }
 
+/**
+ * @brief Wait for an I2C event, or time out in case of error.
+ * @param dev I2C device
+ * @param state I2C_state state to wait for
+ * @param timeout Timeout, in milliseconds
+ * @return 0 if target state is reached, a negative value on error.
+ */
+extern int32 wait_for_state_change(i2c_dev *dev,
+																	i2c_state state,
+																	uint32 timeout);
+
+/**
+ * @brief Reset an I2C bus.
+ *
+ * Reset is accomplished by clocking out pulses until any hung slaves
+ * release SDA and SCL, then generating a START condition, then a STOP
+ * condition.
+ *
+ * @param dev I2C device
+ */
+extern void i2c_bus_reset(const i2c_dev *dev);
+
+/**
+ * @brief Initialize an I2C device as bus master
+ * @param dev Device to enable
+ * @param flags Bitwise or of the following I2C options:
+ *              I2C_FAST_MODE: 400 khz operation,
+ *              I2C_DUTY_16_9: 16/9 Tlow/Thigh duty cycle (only applicable for
+ *                             fast mode),
+ *              I2C_BUS_RESET: Reset the bus and clock out any hung slaves on
+ *                             initialization,
+ *              I2C_10BIT_ADDRESSING: Enable 10-bit addressing,
+ *              I2C_REMAP: (deprecated, STM32F1 only) Remap I2C1 to SCL/PB8
+ *                         SDA/PB9.
+ */
+extern void i2c_master_enable(i2c_dev *dev, uint32 flags);
+
+/**
+ * @brief Process an i2c transaction.
+ *
+ * Transactions are composed of one or more i2c_msg's, and may be read
+ * or write tranfers.  Multiple i2c_msg's will generate a repeated
+ * start in between messages.
+ *
+ * @param dev I2C device
+ * @param msgs Messages to send/receive
+ * @param num Number of messages to send/receive
+ * @param timeout Bus idle timeout in milliseconds before aborting the
+ *                transfer.  0 denotes no timeout.
+ * @return 0 on success,
+ *         I2C_ERROR_PROTOCOL if there was a protocol error,
+ *         I2C_ERROR_TIMEOUT if the transfer timed out.
+ */
+extern int32 i2c_master_xfer(i2c_dev *dev,
+                      i2c_msg *msgs,
+                      uint16 num,
+                      uint32 timeout);
+
+/**
+ * @brief Fill data register with slave address
+ * @param dev I2C device
+ * @param addr Slave address
+ * @param rw Read/write bit
+ */
+extern void i2c_send_slave_addr(i2c_dev *dev, uint32 addr, uint32 rw);
+
 /* Start/stop conditions */
 
 /**
  * @brief Generate a start condition on the bus.
  * @param dev I2C device
  */
-static inline void i2c_start_condition(i2c_dev *dev) {
-    uint32 cr1;
-    while ((cr1 = dev->regs->CR1) & (I2C_CR1_START |
-                                     I2C_CR1_STOP  |
-                                     I2C_CR1_PEC)) {
-        ;
-    }
-    dev->regs->CR1 |= I2C_CR1_START;
-}
+extern void i2c_start_condition(i2c_dev *dev);
 
 /**
  * @brief Generate a stop condition on the bus
  * @param dev I2C device
  */
-static inline void i2c_stop_condition(i2c_dev *dev) {
-    uint32 cr1;
-    while ((cr1 = dev->regs->CR1) & (I2C_CR1_START |
-                                     I2C_CR1_STOP  |
-                                     I2C_CR1_PEC)) {
-        ;
-    }
-    dev->regs->CR1 |= I2C_CR1_STOP;
-    while ((cr1 = dev->regs->CR1) & (I2C_CR1_START |
-                                     I2C_CR1_STOP  |
-                                     I2C_CR1_PEC)) {
-        ;
-    }
-
-}
+extern void i2c_stop_condition(i2c_dev *dev);
 
 /* IRQ enable/disable */
 
@@ -262,9 +206,6 @@ static inline void i2c_stop_condition(i2c_dev *dev) {
 #define _i2c_irq_priority_fixup(dev) ((void)0)
 #endif
 
-#define I2C_IRQ_ERROR              I2C_CR2_ITERREN
-#define I2C_IRQ_EVENT              I2C_CR2_ITEVTEN
-#define I2C_IRQ_BUFFER             I2C_CR2_ITBUFEN
 /**
  * @brief Enable one or more I2C interrupts
  * @param dev I2C device
@@ -273,10 +214,7 @@ static inline void i2c_stop_condition(i2c_dev *dev) {
  *             I2C_IRQ_EVENT (event interrupt), and
  *             I2C_IRQ_BUFFER (buffer interrupt).
  */
-static inline void i2c_enable_irq(i2c_dev *dev, uint32 irqs) {
-    _i2c_irq_priority_fixup(dev);
-    dev->regs->CR2 |= irqs;
-}
+extern void i2c_enable_irq(i2c_dev *dev, uint32 irqs);
 
 /**
  * @brief Disable one or more I2C interrupts
@@ -286,9 +224,7 @@ static inline void i2c_enable_irq(i2c_dev *dev, uint32 irqs) {
  *             I2C_IRQ_EVENT (event interrupt), and
  *             I2C_IRQ_BUFFER (buffer interrupt).
  */
-static inline void i2c_disable_irq(i2c_dev *dev, uint32 irqs) {
-    dev->regs->CR2 &= ~irqs;
-}
+extern void i2c_disable_irq(i2c_dev *dev, uint32 irqs);
 
 /* ACK/NACK */
 
@@ -296,17 +232,13 @@ static inline void i2c_disable_irq(i2c_dev *dev, uint32 irqs) {
  * @brief Enable I2C acknowledgment
  * @param dev I2C device
  */
-static inline void i2c_enable_ack(i2c_dev *dev) {
-    dev->regs->CR1 |= I2C_CR1_ACK;
-}
+extern void i2c_enable_ack(i2c_dev *dev);
 
 /**
  * @brief Disable I2C acknowledgment
  * @param dev I2C device
  */
-static inline void i2c_disable_ack(i2c_dev *dev) {
-    dev->regs->CR1 &= ~I2C_CR1_ACK;
-}
+extern void i2c_disable_ack(i2c_dev *dev);
 
 /* GPIO control */
 
@@ -335,6 +267,11 @@ extern void i2c_master_release_bus(const i2c_dev *dev);
 
 /* Miscellaneous low-level routines */
 
+/**
+ * @brief Initialize an I2C device and reset its registers to their
+ *        default values.
+ * @param dev Device to initialize.
+ */
 void i2c_init(i2c_dev *dev);
 
 /**
@@ -358,53 +295,7 @@ static inline void i2c_peripheral_disable(i2c_dev *dev) {
  * @param dev I2C device
  * @param byte Byte to write
  */
-static inline void i2c_write(i2c_dev *dev, uint8 byte) {
-    dev->regs->DR = byte;
-}
-
-/**
- * @brief Set input clock frequency, in MHz
- * @param dev I2C device
- * @param freq Frequency, in MHz. This must be at least 2, and at most
- *             the APB frequency of dev's bus. (For example, if
- *             rcc_dev_clk(dev) == RCC_APB1, freq must be at most
- *             PCLK1, in MHz). There is an additional limit of 46 MHz.
- */
-static inline void i2c_set_input_clk(i2c_dev *dev, uint32 freq) {
-#define I2C_MAX_FREQ_MHZ 46
-    ASSERT(2 <= freq && freq <= _i2c_bus_clk(dev) && freq <= I2C_MAX_FREQ_MHZ);
-    uint32 cr2 = dev->regs->CR2;
-    cr2 &= ~I2C_CR2_FREQ;
-    cr2 |= freq;
-    dev->regs->CR2 = freq;
-#undef I2C_MAX_FREQ_MHZ
-}
-
-/**
- * @brief Set I2C clock control register.
- *
- * See the chip reference manual for the details.
- *
- * @param dev I2C device
- * @param val Value to use for clock control register (in
- *            Fast/Standard mode)
- */
-static inline void i2c_set_clk_control(i2c_dev *dev, uint32 val) {
-    uint32 ccr = dev->regs->CCR;
-    ccr &= ~I2C_CCR_CCR;
-    ccr |= val;
-    dev->regs->CCR = ccr;
-}
-
-/**
- * @brief Set SCL rise time
- * @param dev I2C device
- * @param trise Maximum rise time in fast/standard mode (see chip
- *              reference manual for the relevant formulas).
- */
-static inline void i2c_set_trise(i2c_dev *dev, uint32 trise) {
-    dev->regs->TRISE = trise;
-}
+extern void i2c_write(i2c_dev *dev, uint8 byte);
 
 #ifdef __cplusplus
 }
--- libmaple/stm32f1/include/series/i2c.h
+++ libmaple/stm32f1/include/series/i2c.h
@@ -41,7 +41,18 @@
  * Register maps
  */
 
-struct i2c_reg_map;
+/** I2C register map type */
+typedef struct i2c_reg_map {
+    __io uint32 CR1;            /**< Control register 1 */
+    __io uint32 CR2;            /**< Control register 2 */
+    __io uint32 OAR1;           /**< Own address register 1 */
+    __io uint32 OAR2;           /**< Own address register 2 */
+    __io uint32 DR;             /**< Data register */
+    __io uint32 SR1;            /**< Status register 1 */
+    __io uint32 SR2;            /**< Status register 2 */
+    __io uint32 CCR;            /**< Clock control register */
+    __io uint32 TRISE;          /**< TRISE (rise time) register */
+} i2c_reg_map;
 
 /** STM32F1 I2C1 register map base pointer */
 #define I2C1_BASE               ((struct i2c_reg_map*)0x40005400)
@@ -56,9 +67,90 @@ extern i2c_dev* const I2C1;
 extern i2c_dev* const I2C2;
 
 /*
- * For internal use
+ * Register bit definitions
  */
 
+/* Control register 1 */
+
+#define I2C_CR1_SWRST           (1U << 15)    // Software reset
+#define I2C_CR1_ALERT           (1U << 13)    // SMBus alert
+#define I2C_CR1_PEC             (1U << 12)    // Packet error checking
+#define I2C_CR1_POS             (1U << 11)    // Acknowledge/PEC position
+#define I2C_CR1_ACK             (1U << 10)    // Acknowledge enable
+#define I2C_CR1_STOP            (1U << 9)     // Stop generation
+#define I2C_CR1_START           (1U << 8)     // Start generation
+#define I2C_CR1_NOSTRETCH       (1U << 7)     // Clock stretching disable
+#define I2C_CR1_ENGC            (1U << 6)     // General call enable
+#define I2C_CR1_ENPEC           (1U << 5)     // PEC enable
+#define I2C_CR1_ENARP           (1U << 4)     // ARP enable
+#define I2C_CR1_SMBTYPE         (1U << 3)     // SMBus type
+#define I2C_CR1_SMBTYPE_DEVICE  (0U << 3)     //     SMBus type: device
+#define I2C_CR1_SMBTYPE_HOST    (1U << 3)     //     SMBus type: host
+#define I2C_CR1_SMBUS           (1U << 1)     // SMBus mode
+#define I2C_CR1_SMBUS_I2C       (0U << 1)     //     SMBus mode: I2C
+#define I2C_CR1_SMBUS_SMBUS     (1U << 1)     //     SMBus mode: SMBus
+#define I2C_CR1_PE              (1U << 0)     // Peripheral Enable
+
+/* Control register 2 */
+
+#define I2C_CR2_LAST            (1U << 12)    // DMA last transfer
+#define I2C_CR2_DMAEN           (1U << 11)    // DMA requests enable
+#define I2C_CR2_ITBUFEN         (1U << 10)    // Buffer interrupt enable
+#define I2C_CR2_ITEVTEN         (1U << 9)     // Event interupt enable
+#define I2C_CR2_ITERREN         (1U << 8)     // Error interupt enable
+#define I2C_CR2_FREQ            0x3F          // Peripheral input frequency
+
+/* Own address register 1 */
+
+#define I2C_OAR1_ADDMODE        (1U << 15)    // Addressing mode
+#define I2C_OAR1_ADDMODE_7_BIT  (0U << 15)    //     Addressing mode: 7-bit
+#define I2C_OAR1_ADDMODE_10_BIT (1U << 15)    //     Addressing mode: 10-bit
+#define I2C_OAR1_ADD            0x3FF         // Interface address
+
+/* Own address register 2 */
+
+#define I2C_OAR2_ADD2           0xFE          // Interface address
+#define I2C_OAR2_ENDUAL         1U            // Dual addressing mode enable
+
+/* Status register 1 */
+
+#define I2C_SR1_SMBALERT        (1U << 15)    // SMBus alert
+#define I2C_SR1_TIMEOUT         (1U << 14)    // Timeout or Tlow error
+#define I2C_SR1_PECERR          (1U << 12)    // PEC Error in reception
+#define I2C_SR1_OVR             (1U << 11)    // Overrun/underrun
+#define I2C_SR1_AF              (1U << 10)    // Acknowledge failure
+#define I2C_SR1_ARLO            (1U << 9)     // Arbitration lost
+#define I2C_SR1_BERR            (1U << 8)     // Bus error
+#define I2C_SR1_TXE             (1U << 7)     // Data register empty
+#define I2C_SR1_RXNE            (1U << 6)     // Data register not empty
+#define I2C_SR1_STOPF           (1U << 4)     // Stop detection
+#define I2C_SR1_ADD10           (1U << 3)     // 10-bit header sent
+#define I2C_SR1_BTF             (1U << 2)     // Byte transfer finished
+#define I2C_SR1_ADDR            (1U << 1)     // Address sent/matched
+#define I2C_SR1_SB              (1U << 0)     // Start bit
+
+/* Status register 2 */
+
+#define I2C_SR2_PEC             0xFF00        // Packet error checking register
+#define I2C_SR2_DUALF           (1U << 7)     // Dual flag
+#define I2C_SR2_SMBHOST         (1U << 6)     // SMBus host header
+#define I2C_SR2_SMBDEFAULT      (1U << 5)     // SMBus device default address
+#define I2C_SR2_GENCALL         (1U << 4)     // General call address
+#define I2C_SR2_TRA             (1U << 2)     // Transmitter/receiver
+#define I2C_SR2_BUSY            (1U << 1)     // Bus busy
+#define I2C_SR2_MSL             (1U << 0)     // Master/slave
+
+/* Clock control register */
+
+#define I2C_CCR_FS              (1U << 15)    // Fast mode selection
+#define I2C_CCR_DUTY            (1U << 14)    // Fast mode duty cycle
+#define I2C_CCR_DUTY_2_1        (0U << 14)    //     Fast mode duty: 2/1
+#define I2C_CCR_DUTY_16_9       (1U << 14)    //     Fast mode duty: 16/9
+#define I2C_CCR_CCR             0xFFF         // Clock control bits
+
+/*
+ * For internal use
+ */
 static inline uint32 _i2c_bus_clk(i2c_dev *dev) {
     /* Both I2C peripherals are on APB1 */
     return STM32_PCLK1 / (1000 * 1000);
@@ -80,6 +172,54 @@ static inline void _i2c_handle_remap(i2c_dev *dev, uint32 flags) {
         I2C1->sda_pin = 9;
         I2C1->scl_pin = 8;
     }
+}
+
+/**
+ * @brief Set input clock frequency, in MHz
+ * @param dev I2C device
+ * @param freq Frequency, in MHz. This must be at least 2, and at most
+ *             the APB frequency of dev's bus. (For example, if
+ *             rcc_dev_clk(dev) == RCC_APB1, freq must be at most
+ *             PCLK1, in MHz). There is an additional limit of 46 MHz.
+ */
+static inline void i2c_set_input_clk(i2c_dev *dev, uint32 freq) {
+#define I2C_MAX_FREQ_MHZ 46
+    ASSERT(2 <= freq && freq <= _i2c_bus_clk(dev) && freq <= I2C_MAX_FREQ_MHZ);
+    uint32 cr2 = dev->regs->CR2;
+    cr2 &= ~I2C_CR2_FREQ;
+    cr2 |= freq;
+    dev->regs->CR2 = freq;
+#undef I2C_MAX_FREQ_MHZ
+}
+
+/**
+ * @brief Set I2C clock control register.
+ *
+ * See the chip reference manual for the details.
+ *
+ * @param dev I2C device
+ * @param val Value to use for clock control register (in
+ *            Fast/Standard mode)
+ */
+static inline void i2c_set_clk_control(i2c_dev *dev, uint32 val) {
+    uint32 ccr = dev->regs->CCR;
+    ccr &= ~I2C_CCR_CCR;
+    ccr |= val;
+    dev->regs->CCR = ccr;
+}
+
+/* Auxiliary procedure for enabling an I2C peripheral; `flags' as for
+ * i2c_master_enable(). */
+void _i2c_set_ccr_trise(i2c_dev *dev, uint32 flags);
+
+/**
+ * @brief Set SCL rise time
+ * @param dev I2C device
+ * @param trise Maximum rise time in fast/standard mode (see chip
+ *              reference manual for the relevant formulas).
+ */
+static inline void i2c_set_trise(i2c_dev *dev, uint32 trise) {
+    dev->regs->TRISE = trise;
 }
 
 #endif  /* _LIBMAPLE_STM32F1_I2C_H_ */
